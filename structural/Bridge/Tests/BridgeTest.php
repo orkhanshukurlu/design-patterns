c6f6675ff60710ab1aba0ca07a1ae1fa146ea74b
Orkhan Shukurlu <orkhandev@gmail.com>
2025-01-02 17:05:58 +0400

    refactor Bridge design pattern

--- structural/Bridge/Tests/BridgeTest.php
+++ structural/Bridge/Tests/BridgeTest.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 
 namespace Structural\Bridge\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Structural\Bridge\HelloWorldService;
 use Structural\Bridge\HtmlFormatter;
 use Structural\Bridge\PlainTextFormatter;
-use PHPUnit\Framework\TestCase;
 
 class BridgeTest extends TestCase
 {
-    public function testCanPrintUsingThePlainTextFormatter()
+    public function testCanPrintUsingThePlainTextFormatter(): void
     {
         $service = new HelloWorldService(new PlainTextFormatter());
 
         $this->assertSame('Hello World', $service->get());
     }
 
-    public function testCanPrintUsingTheHtmlFormatter()
+    public function testCanPrintUsingTheHtmlFormatter(): void
     {
         $service = new HelloWorldService(new HtmlFormatter());
 
